8adee423dd9bef0bb94b8c4455e87f8297213f64
Moueen-Togarvi <moueenuddintogarvi41@gmail.com>
2026-02-06 15:16:31 +0500

    Add Render deployment configuration
    - Added build.sh script for Render
    - Updated requirements.txt with production dependencies (gunicorn, whitenoise)
    - Added WhiteNoise middleware for static file serving
    - Configured STATIC_ROOT and WhiteNoise storage
    - Ready for Render deployment

--- core/core/settings.py
+++ core/core/settings.py
@@ -53,6 +53,7 @@ AUTH_USER_MODEL = 'base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     "corsheaders.middleware.CorsMiddleware",
 
@@ -144,7 +145,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
-# STATIC_ROOT =
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
